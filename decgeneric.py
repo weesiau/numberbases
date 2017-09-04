87237b3529f0c542af6bb9eae0fad2781ef47f03
weesiau <goh.siauwee@dhs.sg>
2017-09-04 14:59:57 +0800

    Update decgeneric.py

--- decgeneric.py
+++ decgeneric.py
@@ -1,14 +1,23 @@
-def decimal_gen(dec):
+hexabases = {'1':'1','2':'2','3':'3','4':'4','5':'5','6':'6','7':'7',\
+             '8':'8','9':'9','10':'A','11':'B','12':'C','13':'D','14':'E',\
+             '15':'F'}
+
+def decgen(dec):
     num = int(dec)
-    base = int(input("Enter the number base(2,8,16): "))
+    base = int(input("Enter the base you want to convert to."))
+    if base != 2 or 8 or 16:
+        print("cannot convert hehexd")
     result = []
-    while (num > 0):
+    while num > 0:
         rem = num % base
-        result.append(str(rem))
+        if base == 16:
+            result.append(hexabases[str(rem)])
+        else:
+            result.append(str(rem))
         num = num // base
     result.reverse()
     return(''.join(result))
 
 
-# main
-print(decimal_gen(95))
+print(decgen(95))
+
